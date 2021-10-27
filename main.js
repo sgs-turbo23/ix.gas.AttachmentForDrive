9f1198d09d46e997f0a1bb15e2a98b500f1672f8
sgs-turbo23 <masiwasaki1006@gmail.com>
2021-10-27 02:56:38 +0000

    mainの最新化

---
+++ main.js
@@ -0,0 +1,39 @@
+let slack;
+
+function main() {
+  slack = new slackNotifier(secret.getSlackId(), 'Save Attachment');
+  try {
+    const yesterday = datetimeUtil.getYesterday(new Date());
+    var mailList = getMessages(yesterday);
+    console.log(mailList);
+    if (mailList.length > 0) {
+      saveAttachment(yesterday, mailList);
+    }
+  } catch (error) {
+    postToSlack(`エラーが発生しました。\n${error}`);
+  }
+}
+
+function getMessages(date) {
+  // 前日のメールを取得
+  const query = `has:attachment after:${datetimeUtil.makeDateString(date)}`;
+  // メールの構造はthreads > messsages > thread > messageの構造
+  const threads = GmailApp.search(query);
+  return GmailApp.getMessagesForThreads(threads);
+}
+
+function saveAttachment(date, messages) {
+  const rootFolder = DriveApp.getFolderById(secret.getDriveDirId());
+  const folder = rootFolder.createFolder(datetimeUtil.makeShortDateString(date));
+  for(const thread of messages){
+    for(const message of thread){
+      // Attachment（添付ファイルを取得）
+      const attachments = message.getAttachments();
+      // 添付ファイルが存在する場合、全てを取得
+      for(const attachment of attachments){
+        folder.createFile(attachment);
+      }
+    }
+  }
+  postToSlack(`<@ma.iw>Gmailの添付ファイルをGoogle Driveに保存しました\n${folder.getUrl()}`);
+}
